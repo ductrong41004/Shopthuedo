58c0fe6c7297894c629712f261fcd5951efa03e5
Duc Trong <dotangductrong410@gmail.com>
2026-07-19 03:19:56 +0700

    Fix: Enable_trailingSlash_in_Next_config_to_resolve_PWA_404

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/Shopthuedo",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
